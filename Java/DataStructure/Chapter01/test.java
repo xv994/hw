e94a78f81902d0e231105fbb19de75261f082f18
Xvzh <xvzh994@outlook.com>
2021-03-21 15:48:22 +0800

    修改第一次作业

--- Java/DataStructure/Chapter01/test.java
+++ Java/DataStructure/Chapter01/test.java
@@ -8,15 +8,15 @@ public class test {
         
         Scanner input = new Scanner(System.in);
 
-
         while (true){
-            System.out.println("1.add\n2.sub\n3.exit");
+            
             String operator = input.nextLine();
             System.out.print("Please input the first number: ");
             String number1 = input.nextLine();
             System.out.print("Please input the second number: ");
             String number2 = input.nextLine();
             System.out.println("Please choose the kind of operation");
+            System.out.println("1.add\n2.sub\n3.exit");
 
             if (operator.equals("1"))
                 System.out.println("The result is " + new BigInteger(number1).add(new BigInteger(number2)));
